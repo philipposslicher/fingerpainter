23d0fb01a7cf16306fdfbe92de33d924b30de3e5
Philippos Slicher <philipposslicher@gmail.com>
2016-01-14 08:57:15 +0400

    USABLE: not done

--- FingerPainter/CanvasView.swift
+++ FingerPainter/CanvasView.swift
@@ -5,11 +5,13 @@
 //  Created by Philippos Slicher on 1/12/16.
 //  Copyright © 2016 Your School. All rights reserved.
 //
+//File Inactive
+/*
 import UIKit
 import Foundation
 
 class CanvasView: UIView {//
- 
+
     override func drawRect(rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()
         CGContextSetStrokeColorWithColor(context, UIColor.orangeColor().CGColor)
@@ -19,7 +21,26 @@ class CanvasView: UIView {//
         CGContextAddLineToPoint(context, 150, 400)
         CGContextStrokePath(context)
     }
-    
 
+    func drawFromPoint(start: CGPoint, toPoint end: CGPoint){
+        // set the context to that of an image
+        UIGraphicsBeginImageContext(canvas.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        // draw the existing image onto the current context
+        canvas.image?.drawInRect(CGRect(x: 0, y: 0, width: canvas.frame.size.width, height: canvas.frame.size.height))
+        // draw the new line segment
+        CGContextSetLineWidth(context, 5)
+        CGContextSetStrokeColorWithColor(context, UIColor.magentaColor().CGColor)
+        CGContextBeginPath(context)
+        CGContextMoveToPoint(context, start.x, start.y)
+        CGContextAddLineToPoint(context, end.x, end.y)
+        CGContextStrokePath(context)
+        // obtain a UIImage object from the context
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        // Set the UIImageView's image to the new, generated image canvas.image = newImage
+        
+    }
 
 }//
+*/
--- FingerPainter/ViewController.swift
+++ FingerPainter/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var canvas: UIImageView!
+    
+    var start: CGPoint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,18 +23,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        if let touch = touches.first {
-            let point = touch.locationInView(view)
-            // print message with breakpoint here
-        }
+    func drawFromPoint(start: CGPoint, toPoint end: CGPoint){
+        // set the context to that of an image
+        UIGraphicsBeginImageContext(canvas.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        // draw the existing image onto the current context
+        canvas.image?.drawInRect(CGRect(x: 0, y: 0,
+            width: canvas.frame.size.width,
+            height: canvas.frame.size.height))
+        // draw the new line segment
+        CGContextSetLineWidth(context, 5)
+        CGContextSetStrokeColorWithColor(context,
+            UIColor.magentaColor().CGColor)
+        CGContextBeginPath(context)
+        CGContextMoveToPoint(context, start.x, start.y)
+        CGContextAddLineToPoint(context, end.x, end.y)
+        CGContextStrokePath(context)
+        // obtain a UIImage object from the context
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        // Set the UIImageView's image to the new, generated image
+        canvas.image = newImage
+
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        if let touch = touches.first {
-            let point = touch.locationInView(view)
-            // print message with breakpoint here
-        }
+    override func touchesMoved(touches: Set<UITouch>,
+        withEvent event: UIEvent?) {
+            if let touch = touches.first {
+                let end = touch.locationInView(canvas)
+                if let start = self.start {
+                    drawFromPoint(start, toPoint: end)
+                }
+                self.start = end
+            }
+    }
+
+    override func touchesBegan(touches: Set<UITouch>,
+        withEvent event: UIEvent?) {
+            if let touch = touches.first {
+                start = touch.locationInView(canvas)
+            }
     }
 
 }
